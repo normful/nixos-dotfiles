e8dfa5ce0d8d44c4f591d2411e798da4aabca810
Norman Sue <normful@users.noreply.github.com>
2022-12-31 02:55:03 -0800

    alias(fish): add fish functions for syncing android devices

---
+++ fish/.config/fish/functions/epull.fish
@@ -0,0 +1,3 @@
+function epull --wraps='nvim ~/code/dotfiles/bin/_push_and_pull_android.sh' --description 'alias epull nvim ~/code/dotfiles/bin/_push_and_pull_android.sh'
+  nvim ~/code/dotfiles/bin/_push_and_pull_android.sh $argv; 
+end
---
+++ fish/.config/fish/functions/epush.fish
@@ -0,0 +1,3 @@
+function epush --wraps='nvim ~/code/dotfiles/bin/_push_and_pull_android.sh' --description 'alias epush nvim ~/code/dotfiles/bin/_push_and_pull_android.sh'
+  nvim ~/code/dotfiles/bin/_push_and_pull_android.sh $argv; 
+end
