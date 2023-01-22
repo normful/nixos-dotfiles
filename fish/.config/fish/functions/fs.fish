7c589826dd206fe0893469ff0e8a89b5a7d32321
Norman Sue <normful@users.noreply.github.com>
2023-01-22 15:58:18 -0500

    config(fish): change the fs funcsave function to automatically run the ms make stow function
    Why:
    
    So that you don't forget to create the necessary symlinks to enable a newly
    created fish function.

--- fish/.config/fish/functions/fs.fish
+++ fish/.config/fish/functions/fs.fish
@@ -1,3 +1,4 @@
 function fs --description 'Automatically save a new fish function file, after creating an alias'
     funcsave --directory ~/code/nixos-dotfiles/fish/.config/fish/functions $argv
+    ms
 end
