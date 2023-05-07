fe7b8f6e034fc1091c34ac80d6b3ff70d0dd990e
Norman Sue <normful@users.noreply.github.com>
2023-05-07 22:12:50 +0900

    config(nvim): markdownpreview

---
+++ nvim/.config/nvim/lua/plugins/peek-nvim.lua
@@ -0,0 +1,24 @@
+return {
+  'toppair/peek.nvim',
+  build = 'deno task --quiet build:fast',
+  keys = {
+    {
+      '<Leader>mp',
+      function()
+        local peek = require('peek')
+        if peek.is_open() then
+          peek.close()
+        else
+          peek.open()
+          vim.fn.system("osascript -e 'tell application \"Vivaldi\" to set bounds of front window to {793, 38, 1512, 982}'")
+        end
+      end,
+      desc = 'Peek (Markdown Preview)',
+    },
+  },
+  ft = 'markdown',
+  opts = {
+    theme = 'dark',
+    app = 'browser',
+  },
+}
