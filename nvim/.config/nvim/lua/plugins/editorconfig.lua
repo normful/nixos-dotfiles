c5b299952c3c05f447347b7e63ffbf85b96e5cdf
Norman Sue <normful@users.noreply.github.com>
2023-05-07 22:15:38 +0900

    config(nvim): editorconfig

---
+++ nvim/.config/nvim/lua/plugins/editorconfig.lua
@@ -0,0 +1,9 @@
+local function configure_editorconfig()
+  vim.g.EditorConfig_exclude_patterns = {'fugitive://.*'}
+end
+
+return {
+  'editorconfig/editorconfig-vim',
+  config = configure_editorconfig,
+  event = 'VeryLazy',
+}
--- nvim_OLD/lua/todo/editorconfig.lua
+++
@@ -1,8 +0,0 @@
-local function configure()
-  vim.g.EditorConfig_exclude_patterns = {'fugitive://.*'}
-end
-
-return {
-  name = 'editorconfig/editorconfig-vim',
-  configure = configure,
-}
