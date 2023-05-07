6bbd9d62a60e9ff7d4576a9709d1cef295083a39
Norman Sue <normful@users.noreply.github.com>
2023-05-07 22:14:24 +0900

    config(nvim): asyncrun

---
+++ nvim/.config/nvim/lua/plugins/async-run.lua
@@ -0,0 +1,16 @@
+local function configure_async_run()
+  local g = vim.g
+
+  g.asyncrun_open = 20
+  g.asyncrun_status = ''
+end
+
+return {
+  'skywind3000/asyncrun.vim',
+  config = configure_async_run,
+  keys = {
+    -- This is used instead of vim-test
+    { '<Leadef>tf', '<Cmd>AsyncRun -cwd=<root> -mode=terminal -focus=0 -pos=right npm run test "%:p"<CR>' },
+  },
+  lazy = false,
+}
--- nvim_OLD/lua/async-run.lua
+++
@@ -1,8 +0,0 @@
-local function configure_async_run()
-  local g = vim.g
-
-  g.asyncrun_open = 20
-  g.asyncrun_status = ''
-end
-
-configure_async_run()
