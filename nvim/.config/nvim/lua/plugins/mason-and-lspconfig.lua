9230b99ff46509fd6e36169c782fa78e688e6856
Norman Sue <normful@users.noreply.github.com>
2023-05-07 22:16:36 +0900

    config(nvim): mason-and-lspconfig

---
+++ nvim/.config/nvim/lua/plugins/mason-and-lspconfig.lua
@@ -0,0 +1,74 @@
+local function configure_mason_and_lsp_config()
+  local lspconfig = require('lspconfig')
+
+  require('mason').setup()
+
+  require('mason-lspconfig').setup({
+    ensure_installed = {
+      'eslint',
+      'lua_ls',
+      'tsserver',
+      'vimls',
+    },
+  })
+
+  lspconfig.eslint.setup()
+  lspconfig.lua_ls.setup({
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          globals = { 'vim' },
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file('', true),
+          checkThirdParty = false,
+        },
+        telemetry = {
+          enable = false,
+        },
+      }
+    }
+  })
+  lspconfig.tsserver.setup()
+  lspconfig.vimls.setup()
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+      -- Enable completion triggered by <C-x><C-o>
+      vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+      local bufLocalMappingOpts = { buffer = ev.buf }
+
+      -- Docs on below:
+      -- :help vim.lsp.* 
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>dec', vim.lsp.buf.declaration, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>im', vim.lsp.buf.implementation, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>ty', vim.lsp.buf.type_implementation, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>v', vim.lsp.buf.definition, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>f', vim.lsp.buf.references, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>d', vim.lsp.buf.signature_help, bufLocalMappingOpts)
+      vim.keymap.set('n', '<Leader>re', vim.lsp.buf.rename, bufLocalMappingOpts)
+
+      -- NOTE(norman): I have not used the below yet
+      vim.keymap.set('n', '<Leader>fo', function()
+        vim.lsp.buf.format { async = true }
+      end, opts)
+    end,
+  })
+end
+
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason-lspconfig.nvim' },
+  },
+  config = configure_mason_and_lspconfig,
+  build = ':MasonUpdate',
+  lazy = false,
+}
