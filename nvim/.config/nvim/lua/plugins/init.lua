f885000ac7e96144f1cee114e3e59c5d238e4e53
Norman Sue <normful@users.noreply.github.com>
2024-08-12 00:43:16 +0900

    M (modified) nvim/.config/nvim/lua/plugins/init.lua

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,10 +1,12 @@
 return {
 	{
 		'normful/monokai.nvim',
+    -- 'rebelot/kanagawa.nvim',
 		lazy = false,
 		priority = 1000,
-		config = function() 
+		config = function()
 			vim.cmd([[colorscheme monokai]])
+			-- vim.cmd([[colorscheme kanagawa]])
 		end,
 	},
 	{ 'nvim-lua/plenary.nvim', lazy = false },
