f07bc11e00e11d215852027b95e373763b6d449f
Norman Sue <normful@users.noreply.github.com>
2024-11-28 02:19:47 +0900

    config(nvim): install some modern colorschemes, and use folke/styler.nvim to switch between them for different buffers of different filetypes

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,11 +1,130 @@
+--[[
+TODO(norman): Configure any overrides for any particular colorschemes used below
+
+Docs on basic highlight groups
+:h group-name
+:h highlight-groups
+
+Docs on treesitter highlight groups
+:h treesitter-highlight-groups
+
+Show all highlight groups in your current session
+:so $VIMRUNTIME/syntax/hitest.vim
+
+Maybe reuse some of:
+https://github.com/normful/monokai.nvim/blob/4ab525323d91b0ff1e360fb11dece9fa6411f727/lua/monokai.lua#L418-L532
+]]
+
+local function configure_catppuccin()
+  require('catppuccin').setup({
+    flavour = 'mocha',
+    no_italic = true,
+    custom_highlights = {},
+    default_integrations = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = true,
+      mason = true,
+      nvim_surround = true,
+      treesitter_context = true,
+      ufo = true,
+    },
+  })
+end
+
+local function configure_nightfox()
+  require('nightfox').setup({})
+end
+
+local function configure_monokai_nightasty()
+  require('monokai-nightasty').setup({
+    dark_style_background = '#000000',
+  })
+end
+
+local function configure_everforest()
+  require('everforest').setup({
+    background = 'hard',
+    ui_contrast = 'high',
+  })
+end
+
+local function configure_tokyodark()
+  require('tokyodark').setup({})
+end
+
+local function configure_nordic()
+  require('nordic').setup({})
+end
+
+local function configure_styler()
+  local styler = require('styler')
+  styler.setup({
+    themes = {
+      gleam = { colorscheme = 'tokyodark', background = 'dark' },
+      go = { colorscheme = 'catppuccin-mocha', background = 'dark' },
+      lua = { colorscheme = 'terafox', background = 'dark' },
+      markdown = { colorscheme = 'monokai-nightasty', background = 'dark' },
+      norg = { colorscheme = 'duskfox', background = 'dark' },
+      typescript = { colorscheme = 'monokai-nightasty', background = 'dark' },
+      yaml = { colorscheme = 'nordic', background = 'dark' },
+    },
+  })
+
+  -- Set the colorscheme to use for most filetypes by default
+  vim.cmd.colorscheme('monokai-nightasty')
+end
+
 return {
   {
-    'normful/monokai.nvim',
+    'nvim-lua/plenary.nvim',
     lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme monokai]])
-    end,
+    priority = 1200,
   },
-  { 'nvim-lua/plenary.nvim', lazy = false },
+  {
+    'folke/styler.nvim',
+    config = configure_styler,
+    lazy = false,
+    priority = 1100,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1090,
+    config = configure_catppuccin,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    lazy = false,
+    priority = 1080,
+    config = configure_nightfox,
+  },
+  {
+    'polirritmico/monokai-nightasty.nvim',
+    lazy = false,
+    priority = 1070,
+    config = configure_monokai_nightasty,
+  },
+  {
+    'neanias/everforest-nvim',
+    lazy = false,
+    priority = 1060,
+    config = configure_everforest,
+  },
+  {
+    'tiagovla/tokyodark.nvim',
+    lazy = false,
+    priority = 1050,
+    config = configure_tokyodark,
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1040,
+    config = configure_nordic,
+  },
 }
