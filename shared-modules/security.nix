4fdeae67094d44519551dd7c0c04e0f2566e4ea8
Norman Sue <normful@users.noreply.github.com>
2025-09-08 23:11:47 +0900

    feat(nix): introduce `my.bigInstall` option for enhanced package set
    This commit introduces a new Nix option, `my.bigInstall`, which acts as a boolean flag. When set to true, it enables a more comprehensive set of packages and configurations across various modules.
    
    The changes include:
    - Adding `my.bigInstall` to `core.nix` to control system-wide settings.
    - Conditionally enabling `programs.nh` and adding `nix-output-monitor` and `nvd` to system packages via `nh.nix` when `my.bigInstall` is true.
    - Moving the `polkit` configuration to `security.nix` and conditionally enabling it based on `my.bigInstall`. This allows members of the "wheel" group to reboot/shutdown without a password.
    - Conditionally enabling `services.fail2ban` and adjusting its configurations when `my.bigInstall` is true.
    - Moving the `polkit` configuration from `user.nix` to `security.nix` to centralize security-related settings.
    
    Additionally, the Nix garbage collection interval has been shortened from 3 days to 1 hour in `nix.nix` to ensure more frequent cleaning of old Nix store paths.

--- shared-modules/security.nix
+++ shared-modules/security.nix
@@ -1,23 +1,44 @@
 {
   lib,
+  config,
   ...
 }:
 {
+  security.polkit = lib.mkIf config.my.bigInstall {
+    enable = true;
+    # Allow members of the wheel group to shutdown/reboot without password
+    extraConfig = ''
+      polkit.addRule(function(action, subject) {
+          if (
+              subject.isInGroup("wheel") && (
+                  action.id == "org.freedesktop.login1.reboot" ||
+                  action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+                  action.id == "org.freedesktop.login1.power-off" ||
+                  action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+              )
+          )
+          {
+              return polkit.Result.YES;
+          }
+      });
+    '';
+  };
+
   networking.firewall.enable = true;
 
-  services.fail2ban = {
+  services.fail2ban = lib.mkIf config.my.bigInstall {
     enable = true;
     bantime-increment = {
       enable = true;
       multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h";  # 1 week max
+      maxtime = "168h"; # 1 week max
       overalljails = true;
     };
     jails = {
       DEFAULT = {
         settings = {
-          bantime = lib.mkForce 86400;  # 24 hours
-          findtime = lib.mkForce 600;   # 10 minutes
+          bantime = lib.mkForce 86400; # 24 hours
+          findtime = lib.mkForce 600; # 10 minutes
           maxretry = lib.mkForce 2;
         };
       };
