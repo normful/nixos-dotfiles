9f54cc5c019668ab534767d06edb7c2428130da3
Norman Sue <normful@users.noreply.github.com>
2025-09-11 22:43:38 +0900

    refactor(modules/core.nix): move isFirstInstall option definition to core.nix
    The `isFirstInstall` option was previously defined in `install-mode.nix`. This commit moves the definition to `core.nix` as it is a more general option that is not specific to the installation mode. This simplifies the module structure and makes the option more accessible.

--- modules/install-mode.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }:
-{
-  options.my.isFirstInstall = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-    description = "Whether this is the first installation of the system";
-  };
-}
