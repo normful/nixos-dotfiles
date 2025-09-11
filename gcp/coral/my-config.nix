212710ba92414ccee247e3a32451d061c325f8ad
Norman Sue <normful@users.noreply.github.com>
2025-09-11 22:57:18 +0900

    ok

--- gcp/coral/my-config.nix
+++ gcp/coral/my-config.nix
@@ -7,4 +7,6 @@
   my.user.sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL1lqGCwLgpCJ3ge0pVGTj+8k4x7e/6+MT23XrRc9SQd norman_mbp_m1_pro";
 
   system.stateVersion = "24.11"; # Don't change this after initial installation
+
+  my.enableInteractiveCli = true;
 }
