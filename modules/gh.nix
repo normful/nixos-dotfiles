0c108b59b70f7d75dda21b8fed5abba3b4e0a6aa
Norman Sue <normful@users.noreply.github.com>
2025-09-11 19:10:39 +0900

    feat(modules/gh.nix): Add systemd service to automatically add SSH key to GitHub
    This commit introduces a new systemd service in the `gh.nix` module.
    This service is responsible for adding the user's SSH public key to
    their GitHub account using the `gh ssh-key add` command.
    
    The service:
    - Depends on `generate-user-ssh-keys.service` to ensure the SSH key exists.
    - Is configured to run as the specified user.
    - Uses the SSH public key located at `~/.ssh/id_rsa.pub`.
    - Creates a unique title for the SSH key including the username, hostname,
     and current date.
    - Authenticates with GitHub using the GitHub CLI (`gh auth status`).
    - Exits with an error if the SSH public key is not found or if the `gh`
     command fails.
    
    This automation streamlines the process of setting up SSH access to
    GitHub repositories by ensuring the SSH key is consistently added upon
    system initialization.

--- modules/gh.nix
+++ modules/gh.nix
@@ -3,9 +3,12 @@
   pkgs-stable,
   ...
 }:
+let
+  gh-wrapped = pkgs-stable.callPackage ../packages/gh-wrapped { inherit config; };
+in
 {
-  environment.systemPackages = with pkgs-stable; [
-    (callPackage ../packages/gh-wrapped { inherit config; })
+  environment.systemPackages = [
+    gh-wrapped
   ];
 
   sops.secrets = {
@@ -13,5 +16,44 @@
       owner = config.my.user.name;
       group = "users";
     };
+  };
+
+  systemd.services.add-ssh-key-to-github = {
+    description = "Add SSH public key to GitHub account";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "generate-user-ssh-keys.service" ];
+    requires = [ "generate-user-ssh-keys.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = config.my.user.name;
+      Group = "users";
+      RemainAfterExit = true;
+    };
+    script = ''
+      SSH_DIR="/home/${config.my.user.name}/.ssh"
+      PUBLIC_KEY="$SSH_DIR/id_rsa.pub"
+
+      if [ ! -f "$PUBLIC_KEY" ]; then
+        echo "SSH public key not found at $PUBLIC_KEY"
+        exit 1
+      fi
+
+      if ! gh auth status >/dev/null 2>&1; then
+        echo "GitHub CLI gh is not authenticated."
+        exit 1
+      fi
+
+      KEY_TITLE="${config.my.user.name}@${config.my.hostname}-$(date +%Y%m%d)"
+
+      echo "Adding SSH key to GitHub with title: $KEY_TITLE"
+      gh ssh-key add "$PUBLIC_KEY" --title "$KEY_TITLE" --type "authentication"
+
+      if [ $? -eq 0 ]; then
+        echo "SSH key successfully added to GitHub"
+      else
+        echo "Failed to add SSH key to GitHub"
+        exit 1
+      fi
+    '';
   };
 }
