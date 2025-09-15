734aadd6be651f68aa57c2d76b9edad3efc9615d
Norman Sue <normful@users.noreply.github.com>
2025-09-15 23:32:50 +0900

    feat(wsl/duro/my-config.nix): enable language support and docker
    Enables support for TypeScript/JavaScript, Go, Python, Bash, CSS, PHP, and Typst.
    Also enables Docker support for containerized workflows. This improves the development
    environment by providing necessary tooling and runtimes for various programming
    languages and container orchestration.

--- wsl/duro/my-config.nix
+++ wsl/duro/my-config.nix
@@ -2,8 +2,16 @@
 {
   my.user.name = "norman";
   my.hostname = "duro";
+
   my.enableInteractiveCli = true;
-  my.enableFullNeovim = true;
+  my.enableLangTsJs = true;
+  my.enableLangGo = true;
+  my.enableLangPython = true;
+  my.enableLangBash = true;
+  my.enableLangCss = true;
+  my.enableLangPhp = true;
+  my.enableLangTypst = true;
+  my.enableDocker = true;
 
   system.stateVersion = "25.05"; # Don't change this after initial installation
 }
