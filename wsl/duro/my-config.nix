628405e42177fcfd37e76b48ec7d3afd73cbda60
Norman Sue <normful@users.noreply.github.com>
2025-09-15 22:58:19 +0900

    feat(wsl/duro/my-config.nix): enable full neovim support
    Enables the full Neovim configuration, including language servers and other plugins, for a more capable editing experience.

--- wsl/duro/my-config.nix
+++ wsl/duro/my-config.nix
@@ -3,6 +3,7 @@
   my.user.name = "norman";
   my.hostname = "duro";
   my.enableInteractiveCli = true;
+  my.enableFullNeovim = true;
 
   system.stateVersion = "25.05"; # Don't change this after initial installation
 }
