c2323bcb866644766e8ce14b5fd076cace913692
Norman Sue <normful@users.noreply.github.com>
2025-09-15 21:44:14 +0900

    feat(flake.nix): Add NixOS on WSL configuration for Duro VM
    This commit introduces configuration for running NixOS within the Windows Subsystem for Linux (WSL), specifically for a VM named "duro". It includes:
    
    - Adding the `nixos-wsl` flake input, pinned to the `release-25.05` branch.
    - Defining a new `nixosSystem2505` function to create the nixosConfiguration for Duro.
    - Specifying the use of `nixos-wsl.nixosModules.default` and custom modules for the Duro configuration.
    - Establishing a new file `wsl/duro/configuration.nix` to define the WSL system's imports, including `nixos-wsl` and custom modules.
    - Creating a corresponding `wsl/duro/my-config.nix` to set user-specific configurations for the Duro WSL environment, such as enabling WSL, setting the default user, and configuring interactive CLI.
    
    This change allows for NixOS to be managed within a WSL environment, leveraging the Nix package manager for a reproducible and efficient development workflow on Windows.

--- flake.nix
+++ flake.nix
@@ -15,6 +15,9 @@
 
     nix-darwin.url = "github:nix-darwin/nix-darwin/15f067638e2887c58c4b6ba1bdb65a0b61dc58c5";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs-unstable-2511";
+
+    nixos-wsl-2505.url = "github:nix-community/nixos-wsl/release-25.05";
+    nixos-wsl-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
   };
 
   outputs =
@@ -26,24 +29,28 @@
     }:
     let
       genAttrs = nixpkgs-2411.lib.genAttrs;
-      nixosSystem = nixpkgs-2411.lib.nixosSystem;
+      nixosSystem2411 = nixpkgs-2411.lib.nixosSystem;
+      nixosSystem2505 = nixpkgs-2505.lib.nixosSystem;
       darwinSystem = inputs.nix-darwin.lib.darwinSystem;
 
-      linuxHostnames = [
+      gcpLinuxHostnames = [
         "coral"
       ];
 
-      vmConfigs = genAttrs linuxHostnames (name: {
+      gcpVmConfigs = genAttrs gcpLinuxHostnames (name: {
         system = "x86_64-linux";
       });
 
       createGcpConfig =
         hostname: configFile:
         let
-          vmConfig = vmConfigs.${hostname};
+          vmConfig = gcpVmConfigs.${hostname};
         in
-        nixosSystem rec {
+        nixosSystem2411 rec {
           system = vmConfig.system;
+          modules = [
+            ./gcp/${hostname}/${configFile}
+          ];
           specialArgs = {
             inherit inputs;
             pkgs-stable = import nixpkgs-2411 {
@@ -55,17 +62,36 @@
               config.allowUnfree = true;
             };
           };
-          modules = [
-            ./gcp/${hostname}/${configFile}
-          ];
         };
     in
     {
       nixosConfigurations = {
+        # NixOS on Google Cloud Platform virtual machines
         coral = createGcpConfig "coral" "configuration.nix";
         coral-first-install = createGcpConfig "coral" "first-install-configuration.nix";
+
+        # NixOS on Windows, in Windows Subsystem for Linux
+        duro = nixosSystem2505 rec {
+          system = "x86_64-linux";
+          modules = [
+            ./wsl/duro/configuration.nix
+            inputs.nixos-wsl.nixosModules.default
+          ];
+          specialArgs = {
+            inherit inputs;
+            pkgs-stable = import nixpkgs-2505 {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            pkgs-pinned-unstable = import nixpkgs-unstable-2511 {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          };
+        };
       };
 
+      # Nix on macOS using nix-darwin
       darwinConfigurations.cyan = darwinSystem rec {
         system = "aarch64-darwin";
         modules = [
