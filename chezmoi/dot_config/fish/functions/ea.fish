14de81a5d8d97d963b9e1996910413941e9080c6
Norman Sue <normful@users.noreply.github.com>
2025-09-17 00:18:41 +0900

    refactor(fish): remove unused fish functions 'ea' and 'eplans' These functions were no longer being used and were cluttering the configuration. Removing them cleans up the fish configuration.

--- chezmoi/dot_config/fish/functions/ea.fish
+++
@@ -1,6 +0,0 @@
-function ea --description 'Print reminder for how to make fish aliases'
-    echo 'To create new fish aliases and functions:'
-    echo
-    echo "alias mc 'echo Merry Christmas'"
-    echo "fs mc"
-end
--- chezmoi/dot_config/fish/functions/eplans.fish
+++
@@ -1,3 +0,0 @@
-function eplans --wraps='nvim ~/code/drafts/ideas/2020s-plans.md' --description 'alias eplans nvim ~/code/drafts/ideas/2020s-plans.md'
-  nvim ~/code/drafts/ideas/2020s-plans.md $argv; 
-end
