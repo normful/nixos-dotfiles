ad9e4f12dff48d81febffc4834b8868afb276c6c
Norman Sue <normful@users.noreply.github.com>
2025-09-17 01:17:50 +0900

    refactor(t.fish): remove --git-ignore and --icons=always from eza command
    The `--git-ignore` flag will be removed as it is not necessary for the intended functionality. The `--icons=always` flag will also be removed as it can cause issues with certain terminal configurations.

--- chezmoi/dot_config/fish/functions/t.fish
+++ chezmoi/dot_config/fish/functions/t.fish
@@ -1,3 +1,3 @@
 function t --description 'Run eza in tree mode (to replace tree)'
-    eza --tree --classify=auto --color=always --icons=always --almost-all --git-ignore --long --no-permissions --bytes --no-user --no-time --total-size $argv
+    eza --tree --classify=auto --color=always --almost-all --long --no-permissions --bytes --no-user --total-size $argv
 end
