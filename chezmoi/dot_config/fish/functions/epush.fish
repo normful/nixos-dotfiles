58e09778415929e17757a50aa2f7ee1527921074
Norman Sue <normful@users.noreply.github.com>
2025-09-17 00:18:31 +0900

    refactor(fish): consolidate epush and epull fish functions into config.fish.tmpl
    The previous implementation used separate fish functions for `epush` and `epull`. This commit consolidates these into `config.fish.tmpl` and uses aliases instead of functions. This simplifies the configuration by reducing the number of files and makes the aliases more discoverable within the main fish configuration. The functionality remains the same, simply opening the `_push_and_pull_android.sh` script in Neovim.

--- chezmoi/dot_config/fish/functions/epush.fish
+++
@@ -1,3 +0,0 @@
-function epush --wraps='nvim ~/code/dotfiles/bin/_push_and_pull_android.sh' --description 'alias epush nvim ~/code/dotfiles/bin/_push_and_pull_android.sh'
-  nvim ~/code/dotfiles/bin/_push_and_pull_android.sh $argv; 
-end
