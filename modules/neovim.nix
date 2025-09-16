656d910f35058bcdea67b09957329a37ddb67735
Norman Sue <normful@users.noreply.github.com>
2025-09-17 00:47:17 +0900

    refactor(nix): consolidate neovim package configurations
    This commit restructures the Nix configuration to group package installations by language. It introduces a new `enableLangC` option to manage C and C++ development tools.
    
    Key changes include:
    - Moving language-specific packages like `gcc`, `gnumake`, `cargo`, `deno`, `go`, `php`, `composer`, `python`, and `pip` into conditional blocks based on new language enablement options.
    - Adding `wget` and `unzip` to the `interactive-cli` module.
    - Standardizing the inclusion of `stylua`, `tree-sitter`, `php`, `php84Packages.composer`, and `php84Packages.php-cs-fixer` in the Neovim configuration.
    - Updating the `optional-packages-without-config` module to correctly include packages based on language flags.
    - Introducing the `enableLangC` option in `overall-options.nix` to control the installation of C/C++ development tools.

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,10 +1,12 @@
 {
   config,
+  lib,
   pkgs-pinned-unstable,
   ...
 }:
 
 let
+  optionals = lib.optionals;
   fullNeovim = pkgs-pinned-unstable.neovim.override {
     # See https://github.com/NixOS/nixpkgs/blob/a0dfb2256cd7d89ce14c214017e6107c866469d4/pkgs/applications/editors/neovim/wrapper.nix
 
@@ -26,22 +28,26 @@ in
       if config.my.enableFullNeovim then
         [ fullNeovim ]
         ++ (with pkgs-pinned-unstable; [
-          cargo
-          deno
-          gcc
-          gnumake
-          go
           luajit
           luajitPackages.luarocks
-          php
-          php84Packages.composer
-          python313
-          python313Packages.pip
           stylua
           tree-sitter
-          unzip
-          wget
         ])
+        ++ (optionals (!config.my.enableLangTsJs) (with pkgs-pinned-unstable; [ deno ]))
+        ++ (optionals (!config.my.enableLangRust) (with pkgs-pinned-unstable; [ cargo ]))
+        ++ (optionals (!config.my.enableLangGo) (with pkgs-pinned-unstable; [ go ]))
+        ++ (optionals (!config.my.enableLangPhp) (with pkgs-pinned-unstable; [
+          php
+          php84Packages.composer
+        ]))
+        ++ (optionals (!config.my.enableLangPython) (with pkgs-pinned-unstable; [
+          python313
+          python313Packages.pip
+        ]))
+        ++ (optionals (!config.my.enableLangC) (with pkgs-pinned-unstable; [
+          gcc
+          gnumake
+        ]))
       else
         [ pkgs-pinned-unstable.neovim ];
   };
