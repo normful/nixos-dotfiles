db80c0d83e19abe0738d322b62f3fe897ef95f99
Norman Sue <normful@users.noreply.github.com>
2025-09-17 00:19:55 +0900

    refactor(fish/functions): remove deprecated mm and fs functions The `mm` and `fs` fish functions were used for managing NixOS configurations and saving fish functions, respectively. These functions are no longer necessary as their functionality has been superseded by the `chezmoi` tool and other improved workflows. Removing them simplifies the configuration and reduces technical debt.

--- chezmoi/dot_config/fish/functions/fs.fish
+++
@@ -1,4 +0,0 @@
-function fs --description 'Automatically save a new fish function file, after creating an alias'
-    funcsave --directory ~/code/nixos-dotfiles/chezmoi/dot_config/fish/functions $argv
-    ms
-end
