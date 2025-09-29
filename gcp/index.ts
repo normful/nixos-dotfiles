8a53a5928bd370798e23b72b336b5e2a0076b1dc
Norman Sue <normful@users.noreply.github.com>
2025-09-29 11:58:15 +0900

    fix(Pulumi.jute.yaml): update gcp zone from asia-northeast1-b to asia-northeast1-c
    fix(Pulumi.jute.yaml): update machine types and boot disk size for NixOS GCE instances
    fix(gcp/index.ts): reorder exports to maintain alphabetical order
    fix(secrets/gcp_jute.yaml): update tailscale auth key and lastmodified date

--- gcp/index.ts
+++ gcp/index.ts
@@ -2,20 +2,21 @@ export { projectId } from "./config";
 
 import "./network";
 import "./artifact-registry";
-import "./nat-ip-manager-scheduler";
 export {
   artifactRegistryRepositoryUrl,
   artifactRegistryServiceAccountKeyValue,
 } from "./artifact-registry";
+import "./nat-ip-manager-scheduler";
 
 import "./compute";
-import "./firewall";
-import "./iap-iam";
-import "./login-alerts";
-import "./instance-start-stop-alerts";
 export {
   deployedZone,
   instanceName,
   sshRootCommand,
   sshUserCommand,
+  consoleUrl,
 } from "./compute";
+import "./firewall";
+import "./iap-iam";
+import "./login-alerts";
+import "./instance-start-stop-alerts";
