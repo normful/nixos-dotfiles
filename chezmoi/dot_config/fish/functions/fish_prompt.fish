90cb312731f9ad964e9fa26f5776241f56097a01
Norman Sue <normful@users.noreply.github.com>
2026-02-07 16:27:41 +0900

    refactor(fish_prompt): use fish_git_prompt instead of fish_vcs_prompt
    Replaces `fish_vcs_prompt` with `fish_git_prompt` for Git repository status display in the fish shell prompt. This provides a more specific and potentially better-featured Git status indication.

--- chezmoi/dot_config/fish/functions/fish_prompt.fish
+++ chezmoi/dot_config/fish/functions/fish_prompt.fish
@@ -25,6 +25,6 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_status $status_color "[" $last_status "]" $normal
     end
 
-    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_git_prompt) $normal ' ' $prompt_status
     echo -n -s $status_color $suffix ' ' $normal
 end
