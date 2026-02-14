8bd80f93f8d16d728b71e0e21ceb1118e6141d08
Norman Sue <normful@users.noreply.github.com>
2026-02-14 16:37:48 +0900

    M (modified) mac/cyan/configuration.nix

--- mac/cyan/configuration.nix
+++ mac/cyan/configuration.nix
@@ -94,7 +94,6 @@
         goreleaser
         (callPackage ../../packages/beads { })
         (callPackage ../../packages/grepai { })
-        (callPackage ../../packages/gastown { })
         gh # Log into this one manually, unlike the one using GH_TOKEN env var in packages/gh-wrapped/default.nix
         direnv
       ]
