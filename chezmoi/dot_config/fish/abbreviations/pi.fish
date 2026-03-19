f1d432e55319bba402dd55d4b7bc4bc619a63f1c
Norman Sue <normful@users.noreply.github.com>
2026-03-19 23:09:16 +0900

    feat(fish): update 'pi' abbreviations with new models and add utilities feat(fish): add completion file for 'artisan' command chore(fish): add new fish configuration templates for abbreviations and tools

--- chezmoi/dot_config/fish/abbreviations/pi.fish
+++ chezmoi/dot_config/fish/abbreviations/pi.fish
@@ -1,10 +1,14 @@
 abbr --add pim "pi --models '"\
 "aihubmix/cc-minimax-m2.5," \
+"aihubmix/cc-minimax-m2.5-highspeed," \
 "aihubmix/cc-glm-5," \
 "aihubmix/kimi-k2.5," \
+"aihubmix/gpt-5.3-codex," \
+"aihubmix/gemini-3.1-pro-preview-customtools," \
 "opencode/trinity-large-preview-free," \
-"openrouter/arcee-ai/trinity-large-preview:free," \
+"openrouter/openrouter/aurora-alpha," \
 "openrouter/stepfun/step-3.5-flash:free," \
-"openrouter/@preset/kimi25-high-reasoning," \
-"cerebras/qwen-3-235b-a22b-instruct-2507," \
-"google/gemini-3-flash-preview'" \
+"openrouter/arcee-ai/trinity-large-preview:free," \
+"openrouter/arcee-ai/trinity-mini:free," \
+"cerebras/llama3.1-8b," \
+"cerebras/gpt-oss-120b'" \
