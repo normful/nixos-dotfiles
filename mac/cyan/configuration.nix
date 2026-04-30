34e6ff5aaefbdfc2356534aebc242572a50127c8
Norman Sue <normful@users.noreply.github.com>
2026-04-30 18:57:11 +0900

    M (modified) mac/cyan/configuration.nix

--- mac/cyan/configuration.nix
+++ mac/cyan/configuration.nix
@@ -99,6 +99,7 @@
         dolt
         tuios
         sqlite-web
+        skimpdf
       ]
       ++ (with inputs.nix-casks.packages.${pkgs.stdenv.hostPlatform.system}; [
         ### Some of these are gigantic and slow down system rebuild, so I'm purposely just installing them imperatively, outside of Nix
