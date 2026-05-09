7524a0ec58541a33e39b2af9b282c0302e4ac6f3
Norman Sue <normful@users.noreply.github.com>
2026-05-09 22:10:25 +0900

    M (modified) mac/cyan/configuration.nix

--- mac/cyan/configuration.nix
+++ mac/cyan/configuration.nix
@@ -99,6 +99,9 @@
         dolt
         tuios
         sqlite-web
+
+        dotnet-sdk_10
+        libreoffice
       ]
       ++ (with inputs.nix-casks.packages.${pkgs.stdenv.hostPlatform.system}; [
         ### Some of these are gigantic and slow down system rebuild, so I'm purposely just installing them imperatively, outside of Nix
