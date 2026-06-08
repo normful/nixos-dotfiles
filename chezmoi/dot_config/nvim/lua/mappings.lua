5f1c3e1c031955e84dcbb5f447ea410ae7841f9f
Norman Sue <normful@users.noreply.github.com>
2026-06-08 09:23:06 +0900

    ok

--- chezmoi/dot_config/nvim/lua/mappings.lua
+++ chezmoi/dot_config/nvim/lua/mappings.lua
@@ -1,200 +1,135 @@
 local map = vim.keymap.set
+-- file-specific quick-open: <Leader>e<key> → open config file
+local config_files = {
+  m = '~/code/nixos-dotfiles/mac/cyan/configuration.nix',
+  d = '~/code/nixos-dotfiles/wsl/duro/my-config.nix',
+  g = '~/code/nixos-dotfiles/chezmoi/dot_gitconfig.tmpl',
+  w = '~/code/nixos-dotfiles/chezmoi/dot_config/wezterm/wezterm.lua.tmpl',
+  k = '~/code/nixos-dotfiles/chezmoi/dot_config/kitty/kitty.conf',
+  t = '~/code/nixos-dotfiles/chezmoi/dot_config/ghostty/config',
+  f = '~/code/nixos-dotfiles/chezmoi/dot_config/fish/config.fish.tmpl',
+}
+for key, path in pairs(config_files) do
+  map('n', '<Leader>e' .. key, '<Cmd>100vsplit ' .. path .. '<CR>', { desc = 'Edit ' .. key .. ' config' })
+end
 
-------------------------------------------------
--- Disable Arrow Keys in Normal and Insert modes
-------------------------------------------------
+-- arrow keys: disabled in insert mode
+map({ 'i' }, '<Up>', '<Cmd>echoerr "Use k instead"<CR>', { desc = 'Disable <Up>' })
+map({ 'i' }, '<Down>', '<Cmd>echoerr "Use j instead"<CR>', { desc = 'Disable <Down>' })
+map({ 'i' }, '<Left>', '<Cmd>echoerr "Use h instead"<CR>', { desc = 'Disable <Left>' })
+map({ 'i' }, '<Right>', '<Cmd>echoerr "Use l instead"<CR>', { desc = 'Disable <Right>' })
 
-map({ 'i' }, '<Up>', '<Cmd>echoerr "Use k instead"<CR>', { desc = 'Disable <Up>: Use k' })
-map({ 'i' }, '<Down>', '<Cmd>echoerr "Use j instead"<CR>', { desc = 'Disable <Down>: Use j' })
-map({ 'i' }, '<Left>', '<Cmd>echoerr "Use h instead"<CR>', { desc = 'Disable <Left>: Use h' })
-map({ 'i' }, '<Right>', '<Cmd>echoerr "Use l instead"<CR>', { desc = 'Disable <Right>: Use l' })
+-- visual mode: wrapped-line navigation
+map('x', 'j', 'gj', { desc = 'Visual line down' })
+map('x', 'k', 'gk', { desc = 'Visual line up' })
+map('x', '^', 'g^', { desc = 'Visual line first char' })
+map('x', '0', 'g0', { desc = 'Visual line column 0' })
+map('x', '$', 'g$', { desc = 'Visual line end' })
 
-------------------------------------------------
--- Visual Mode Mappings (Mode 'x')
-------------------------------------------------
-
--- Temporarily disabling because of conflict with Neorg
----- map('x', '<', '<gv', { desc = 'Indent line without exiting visual mode' })
----- map('x', '>', '>gv', { desc = 'Unindent line without exiting visual mode' })
-
--- Filetype-specific visual line mappings
--- These mappings scoped to specific filetypes to avoid polluting global keybindings
--- Created for markdown: select lines with V (visual line mode), then <Leader>q
-local augroup_markdown = vim.api.nvim_create_augroup('MarkdownMappings', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'markdown',
-  group = augroup_markdown,
-  callback = function()
-    -- Prepend "> " to selected lines for markdown blockquote
-    -- gv reselects last visual selection because mode='x' exits visual mode before
-    -- the callback runs, and '<,'> marks may be stale without explicit reselection
-    map('x', '<Leader>q', function()
-      vim.cmd('normal! gv')
-      vim.cmd("'<,'>s/^/> /")
-      vim.cmd('nohlsearch')
-    end, { desc = 'Quote selected lines (prepend > )' })
-
-    -- Prepend "- " to selected lines for markdown unordered list items
-    map('x', '<Leader>l', function()
-      vim.cmd('normal! gv')
-      vim.cmd("'<,'>s/^/- /")
-      vim.cmd('nohlsearch')
-    end, { desc = 'Listify selected lines (prepend - )' })
-  end,
-})
-
--- Navigate wrapped lines in Visual mode
-map('x', 'j', 'gj', { desc = 'Move down by visual line' })
-map('x', 'k', 'gk', { desc = 'Move up by visual line' })
-map('x', '^', 'g^', { desc = 'Move to visual beginning of line' })
-map('x', '0', 'g0', { desc = 'Move to visual beginning of line' })
-map('x', '$', 'g$', { desc = 'Move to visual end of line' })
-
-------------------------------------------------
--- Command-line Mode Mappings (Mode 'c')
-------------------------------------------------
-
-map('c', '<C-j>', '<Down>', { desc = 'Navigate down in command line history/completion' })
-map('c', '<C-k>', '<Up>', { desc = 'Navigate up in command line history/completion' })
-map('c', '<C-h>', '<S-left>', { desc = 'Move cursor left' })
-map('c', '<C-l>', '<S-right>', { desc = 'Move cursor right' })
+-- command-line mode: ctrl navigation
+map('c', '<C-j>', '<Down>', { desc = 'Cmdline history down' })
+map('c', '<C-k>', '<Up>', { desc = 'Cmdline history up' })
+map('c', '<C-h>', '<S-left>', { desc = 'Cmdline cursor left' })
+map('c', '<C-l>', '<S-right>', { desc = 'Cmdline cursor right' })
 map(
   'c',
   'w!!',
   'execute "silent! write !SUDO_ASKPASS=`which ssh-askpass` sudo tee % >/dev/null" <bar> edit!',
-  { desc = 'Write buffer using sudo even after forgetting to start nvim with sudo' }
+  { desc = 'Sudo write' }
 )
 
-------------------------------------------------
--- Terminal Mode Mappings (Mode 't')
-------------------------------------------------
-
+-- terminal mode
 map({ 't', 'n' }, '<Localleader>t', '<Cmd>ToggleNvchadTerminal<CR>', { desc = 'Toggle terminal' })
-map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Enter Normal mode from terminal' })
-map('t', '<C-w>', '<C-\\><C-n><C-w>w', { desc = 'Enter Normal mode and switch window' })
+map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Terminal normal mode' })
+map('t', '<C-w>', '<C-\\><C-n><C-w>w', { desc = 'Terminal next window' })
 
-------------------------------------------------
--- Normal Mode Mappings (Mode 'n')
-------------------------------------------------
-
-map(
-  'n',
-  'Q',
-  '<Cmd>echoerr "Q enters command-line mode usually, but has been disabled"<CR>',
-  { desc = 'Disable Q (Ex mode: persistent command line mode)' }
-)
-
-map('n', 'D', '<Cmd>echoerr "Use [Right Command + d] to scroll down"<CR>', { desc = 'Disable D: Use Cmd+j/d to scroll' })
-map('n', 'U', '<Cmd>echoerr "Use [Right Command + u] to scroll up"<CR>', { desc = 'Disable U: Use Cmd+k/u to scroll' })
+-- normal mode: disable noisy keys
+map('n', 'Q', '<Cmd>echoerr "Q disabled"<CR>', { desc = 'Disable Q' })
+map('n', 'D', '<Cmd>echoerr "Use Cmd+d to scroll"<CR>', { desc = 'Disable D' })
+map('n', 'U', '<Cmd>echoerr "Use Cmd+u to scroll"<CR>', { desc = 'Disable U' })
 
 map('n', 'Y', 'yy', { desc = 'Yank whole line' })
-
-map('n', '<Leader>em', '<Cmd>100vsplit ~/code/nixos-dotfiles/mac/cyan/configuration.nix<CR>', { desc = 'Edit cyan Nix config' })
-map('n', '<Leader>ed', '<Cmd>100vsplit ~/code/nixos-dotfiles/wsl/duro/my-config.nix<CR>', { desc = 'Edit duro Nix config' })
-
-map('n', '<Leader>eg', '<Cmd>100vsplit ~/code/nixos-dotfiles/chezmoi/dot_gitconfig.tmpl<CR>', { desc = 'Edit gitconfig' })
-map(
-  'n',
-  '<Leader>ew',
-  '<Cmd>100vsplit ~/code/nixos-dotfiles/chezmoi/dot_config/wezterm/wezterm.lua.tmpl<CR>',
-  { desc = 'Edit wezterm config' }
-)
-map(
-  'n',
-  '<Leader>ek',
-  '<Cmd>100vsplit ~/code/nixos-dotfiles/chezmoi/dot_config/kitty/kitty.conf<CR>',
-  { desc = 'Edit kitty config' }
-)
-map(
-  'n',
-  '<Leader>et',
-  '<Cmd>100vsplit ~/code/nixos-dotfiles/chezmoi/dot_config/ghostty/config<CR>',
-  { desc = 'Edit ghostty config' }
-)
-map(
-  'n',
-  '<Leader>ef',
-  '<Cmd>100vsplit ~/code/nixos-dotfiles/chezmoi/dot_config/fish/config.fish.tmpl<CR>',
-  { desc = 'Edit fish config' }
-)
-
-if vim.g.neovide then
-  map('n', '<D-d>', '20j', { desc = '[Cmd+d] Scroll down 20 lines' })
-  map('n', '<D-u>', '20k', { desc = '[Cmd+u] Scroll up 20 lines' })
-  map('n', '<D-w>', '<C-w>w', { desc = '[Cmd+w] Next window' })
-  map('n', '<D-b>', '<Cmd>bnext<CR>', { desc = '[Cmd+b] Next buffer' })
-end
-
--- These rely mappings in my terminals that send these function keys
-map('n', '<F13>', '20j', { desc = '[Cmd+d] Scroll down 20 lines' })
-map('n', '<F14>', '20k', { desc = '[Cmd+u] Scroll up 20 lines' })
-map('n', '<F15>', '<C-w>w', { desc = '[Cmd+w] Next window' })
-map('n', '<F16>', '<Cmd>bnext<CR>', { desc = '[Cmd+b] Next buffer' })
-
-map('n', '<C-S-j>', '<Cmd>lnext<CR>', { desc = 'Go to next location list item' })
-map('n', '<C-S-k>', '<Cmd>lprevious<CR>', { desc = 'Go to previous location list item' })
-
--- map('n', '/', '/\\v', { desc = "Search with 'very magic' regex" })
-
+map('n', '<C-S-j>', '<Cmd>lnext<CR>', { desc = 'Location list next' })
+map('n', '<C-S-k>', '<Cmd>lprevious<CR>', { desc = 'Location list prev' })
 map('n', 'zl', 'zL', { desc = 'Big scroll right' })
 map('n', 'zh', 'zH', { desc = 'Big scroll left' })
 
-map('n', '<Localleader>h', '<Cmd>set hlsearch!<CR>', { desc = 'Toggle search results highlight' })
-map('n', '<Localleader>w', '<Cmd>set wrap!<CR>', { desc = 'Toggle line wrapping' })
-
+-- <Localleader> mappings
+map('n', '<Localleader>h', '<Cmd>set hlsearch!<CR>', { desc = 'Toggle hlsearch' })
+map('n', '<Localleader>w', '<Cmd>set wrap!<CR>', { desc = 'Toggle wrap' })
 map('n', '<Localleader><Tab>', ':call normful#SetTabSettings()<CR>', { desc = 'Set tab settings' })
-
-map('n', '<Localleader>H', '<Cmd>windo wincmd K<CR>', { desc = 'Rearrange windows in hsplit' })
-map('n', '<Localleader>V', '<Cmd>windo wincmd H<CR>', { desc = 'Rearrange windows in vsplit' })
-
+map('n', '<Localleader>H', '<Cmd>windo wincmd K<CR>', { desc = 'Windows to hsplit' })
+map('n', '<Localleader>V', '<Cmd>windo wincmd H<CR>', { desc = 'Windows to vsplit' })
 map('n', '<Localleader>zt', function()
   require('nvchad.themes').open()
-end, { desc = 'Pick nvchad theme' })
-
+end, { desc = 'Pick theme' })
 map('n', '<Localleader>zc', '<cmd>NvCheatsheet<CR>', { desc = 'Toggle cheatsheet' })
+map('n', '<Localleader>y', '<Cmd>LookupYomitanDefinitions<CR>', { desc = 'Yomitan definitions' })
 
+-- markdown visual: quoting / listifying
+local augroup_md = vim.api.nvim_create_augroup('MarkdownMappings', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  group = augroup_md,
+  callback = function()
+    map('x', '<Leader>q', function()
+      vim.cmd('normal! gv')
+      vim.cmd("'<,'>s/^/> /")
+      vim.cmd('nohlsearch')
+    end, { desc = 'Quote lines (prepend > )' })
+    map('x', '<Leader>l', function()
+      vim.cmd('normal! gv')
+      vim.cmd("'<,'>s/^/- /")
+      vim.cmd('nohlsearch')
+    end, { desc = 'Listify lines (prepend - )' })
+  end,
+})
+
+-- neovim GUI / terminal fallback pairs
+local function neovide_or_term(neovide_lhs, neovide_rhs, desc, term_rhs)
+  if vim.g.neovide then
+    map('n', neovide_lhs, neovide_rhs, { desc = desc })
+  end
+  if term_rhs then
+    map('n', term_rhs[1], term_rhs[2], { desc = term_rhs[3] })
+  end
+end
+neovide_or_term('<D-d>', '20j', '[Cmd+d] Scroll 20 down', { '<F13>', '20j', '[F13] Scroll 20 down' })
+neovide_or_term('<D-u>', '20k', '[Cmd+u] Scroll 20 up', { '<F14>', '20k', '[F14] Scroll 20 up' })
+neovide_or_term('<D-w>', '<C-w>w', '[Cmd+w] Next window', { '<F15>', '<C-w>w', '[F15] Next window' })
+neovide_or_term('<D-b>', '<Cmd>bnext<CR>', '[Cmd+b] Next buffer', { '<F16>', '<Cmd>bnext<CR>', '[F16] Next buffer' })
+
+-- neovim GUI: zoom scale
+if vim.g.neovide then
+  map('n', '<D-=>', function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.25
+  end, { desc = 'Zoom in' })
+  map('n', '<D-->', function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.25
+  end, { desc = 'Zoom out' })
+end
+
+-- git
 local helpers = require('mappings-helpers')
 local open_term = helpers.open_nvchad_term
-local term_id1 = 'term_id1'
+local tid = 'term_id1'
 
--- Commands below that start with :Git need two carriage returns (<CR>) so that
--- 'Press ENTER or type command to continue' is not displayed
+map('n', '<Leader>gb', '<Cmd>normful#GitBlame<CR><F15><CR>', { desc = 'Git blame' })
+map('n', '<Leader>gd', '<Cmd>Git diff<CR>', { desc = 'Git diff' })
+map('n', '<Leader>gw', '<Cmd>NormfulGitStageAllAndCommit<CR>', { desc = 'Git stage+commit (buf dir)' })
+map('n', '<Leader>gsh', '<Cmd>Git show<CR><CR>', { desc = 'Git show' })
+map('n', '<Leader>gs', '<Cmd>Git status<CR>', { desc = 'Git status' })
+map('n', '<Leader>gl', open_term(tid, 'git lforvim "FILEPATH"'), { desc = 'Git log (file)' })
+map('n', '<Leader>gll', open_term(tid, 'git llforvim "FILEPATH"'), { desc = 'Git log w/ stats' })
+map('n', '<Leader>glll', open_term(tid, 'git lllforvim "FILEPATH"'), { desc = 'Git log w/ patches' })
+map('n', '<Leader>gppl', open_term(tid, 'git pplforvim "FILEPATH"'), { desc = 'Git shortlog' })
 
-map('n', '<Leader>gb', '<Cmd>normful#GitBlame<CR><F15><CR>', { desc = 'git blame' })
-
-map('n', '<Leader>gd', '<Cmd>Git diff<CR>', { desc = 'git diff' })
-
-map('n', '<Leader>gw', '<Cmd>NormfulGitStageAllAndCommit<CR>', { desc = 'git stage all and commit (buffer dir)' })
-map('n', '<Leader>gsh', '<Cmd>Git show<CR><CR>', { desc = 'git show' })
-map('n', '<Leader>gs', '<Cmd>Git status<CR>', { desc = 'git status' })
-
-map('n', '<Leader>gl', open_term(term_id1, 'git lforvim "FILEPATH"'), { desc = 'git log this file' })
-map('n', '<Leader>gll', open_term(term_id1, 'git llforvim "FILEPATH"'), { desc = 'git log this file with stats' })
-map('n', '<Leader>glll', open_term(term_id1, 'git lllforvim "FILEPATH"'), { desc = 'git log this file with patches' })
-map('n', '<Leader>gppl', open_term(term_id1, 'git pplforvim "FILEPATH"'), { desc = 'git shortlog' })
+-- zk index
 map('n', '<Leader>zi', '<Cmd>silent !cd $HOME/code/alcove && zk index<CR>', { desc = 'Index zk notes', noremap = true })
 
-map(
-  'n',
-  '<LocalLeader>y',
-  '<Cmd>LookupYomitanDefinitions<CR>',
-  { desc = 'Opens a popup window using dictionary definitions from local Yomitan API server' }
-)
-
+-- debug: capture key code
 map({ 'n', 'i', 'v' }, '<S-F4>', function()
-  print('Debugging mode - press any key (ESC to exit)')
+  print('Debug mode — press any key (ESC to exit)')
   local char = vim.fn.getchar()
-  print('Received keycode: ' .. char .. ' As string: ' .. vim.fn.nr2char(char))
-end)
-
-if vim.g.neovide then
-  local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-  end
-  vim.keymap.set('n', '<D-=>', function()
-    change_scale_factor(1.25)
-  end)
-  vim.keymap.set('n', '<D-->', function()
-    change_scale_factor(1 / 1.25)
-  end)
-end
+  print('Keycode: ' .. char .. ' (' .. vim.fn.nr2char(char) .. ')')
+end, { desc = 'Debug key code' })
